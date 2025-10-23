8fe795737067dac4e4d003691a987826b7b3c219
AlonSakin <29asakin@gannacademy.org>
2025-10-23 11:54:46 -0400

    done 👍👍👍👍👍👍👍👍👍

--- src/municipal_information.py
+++ src/municipal_information.py
@@ -4,7 +4,15 @@ from os import sep
 import pandas as pd
 
 def main():
-    pass 
+    municipalInfo = pd.read_csv("src/municipal.tsv", sep="\t")
+
+    shapeInfo = municipalInfo.shape
+    print(f"Shape: {shapeInfo[0]},{shapeInfo[1]}")
+
+    print("Columns:")
+    columnInfo = municipalInfo.columns
+    for columnName in columnInfo:
+        print(columnName)
 
 
 if __name__ == "__main__":
